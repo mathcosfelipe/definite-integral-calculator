c1c3b66f777646eb48f0a2da5968b3841690c1eb
Marcos Felipe <mathcosfelipe@gmail.com>
2022-07-14 00:43:40 -0300

    Update integral.rs

--- src/integral.rs
+++ src/integral.rs
@@ -1,6 +1,16 @@
-pub(crate) fn calculate(operations: Vec<String>, limits_operations: Vec<Vec<f32>>) {
+// const axes: [String, 2] = ["a"; "b"];
 
-    print!("{:?}", operations);
-    print!("{:?}", limits_operations);
+fn integrate(operations: Vec<String>) -> Vec<String> {
+
+    for operation in 0..operations.len() {
+        println!("{}", operations[operation]);
+    }
+
+    operations
+}
+
+pub(crate) fn calculate(operations: Vec<String>, _limits_operations: Vec<Vec<f32>>) {
+
+    let _operations_integrated: Vec<String> = integrate(operations);
 
 }
