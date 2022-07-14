6cccdfd866a2d18b485e0884e7af8636cd865936
Marcos Felipe <mathcosfelipe@gmail.com>
2022-07-14 10:57:11 -0300

    Update integral.rs

--- src/integral.rs
+++ src/integral.rs
@@ -1,54 +1,57 @@
-use std::any::Any;
-use eval::eval;
+// use std::any::Any;
+// use eval::eval;
 
-pub trait Object {
-    fn type_name(&self) -> &str;
-    fn as_any(&self) -> &dyn Any;
-}
+// pub trait Object {
+//     fn type_name(&self) -> &str;
+//     fn as_any(&self) -> &dyn Any;
+// }
 
-impl Object for f32 {
-    fn type_name(&self) -> &str {"f32"}
-    fn as_any(&self) -> &dyn Any {self}
-}
+// impl Object for f32 {
+//     fn type_name(&self) -> &str {"f32"}
+//     fn as_any(&self) -> &dyn Any {self}
+// }
 
-pub fn is_of_type<T: 'static>(x: &dyn Object) -> bool {
-    x.as_any().is::<T>()
-}
+// pub fn is_of_type<T: 'static>(x: &dyn Object) -> bool {
+//     x.as_any().is::<T>()
+// }
 
-fn integrate(operations: Vec<String>) -> Vec<String> {
+// fn integrate(operations: Vec<String>) -> Vec<String> {
 
-    let mut operations_integrateds: Vec<String> = Vec::new();
+//     let mut operations_integrateds: Vec<String> = Vec::new();
 
-    for operation in 0..operations.len() {
+//     for operation in 0..operations.len() {
         
-        if operations[operation].contains("x") {
+//         if operations[operation].contains("x") {
             
-            if operations[operation] == "-x" {
-                operations_integrateds.push(("-x^2/2").to_string());
-            }
+//             if operations[operation] == "-x" {
+//                 operations_integrateds.push(("-x^2/2").to_string());
+//             }
 
-            if operations[operation] == "x" {
-                operations_integrateds.push(("x^2/2").to_string());
-            }
+//             if operations[operation] == "x" {
+//                 operations_integrateds.push(("x^2/2").to_string());
+//             }
             
-            if is_of_type::<f32>(&operations[operation].parse::<f32>().unwrap()) {
-                operations_integrateds.push((format!("{}x", operations[operation])).to_string());
-            }
+//             if is_of_type::<f32>(&operations[operation].parse::<f32>().unwrap()) {
+//                 operations_integrateds.push((format!("{}x", operations[operation])).to_string());
+//             }
 
-            if operations[operation].contains("^") {
-                // let operation_splited: Split<&str> = operations[operation].to_string().split("^");
-                // println!("{:?}", operation_splited);
-                if is_of_type::<f32>(eval(&operations[operation].to_string()).parse::<f32>().unwrap()) {
-                    // operations_integrateds.push((format!("{}x", operations[operation])).to_string());
-                }
-            }
+//             if operations[operation].contains("^") {
+//                 // let operation_splited: Split<&str> = operations[operation].to_string().split("^");
+//                 // println!("{:?}", operation_splited);
+//                 if is_of_type::<f32>(eval(&operations[operation].to_string()).parse::<f32>().unwrap()) {
+//                     // operations_integrateds.push((format!("{}x", operations[operation])).to_string());
+//                 }
+//             }
 
-        }
+//         }
     
-    }
+//     }
 
-    operations
-}
+//     operations
+// }
+
+mod integrate;
+use integrate::integrate;
 
 pub(crate) fn calculate(operations: Vec<String>, _limits_operations: Vec<Vec<f32>>) {
 
