4e5ced9eb50bf4cad23fc30dfc146cefb457c05f
Marcos Felipe <mathcosfelipe@gmail.com>
2022-07-14 00:29:52 -0300

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 extern crate beginner_tools;
 use beginner_tools::input;
 
+mod integral;
+
 fn main() {
     
     let amount_operations: u32 = loop {
@@ -26,6 +28,7 @@ fn main() {
     let mut limits_operations: Vec<Vec<f32>> = Vec::new();
 
     for limit_input in 1..amount_operations + 1 {
+    
         let inferior_limit: f32 = loop {
             let inferior_limit_message: String = format!("Input the inferior limit of operation nº {}: ", limit_input);
             if let Ok(inferior_limit_message_validity) = input(&inferior_limit_message) {
@@ -33,6 +36,7 @@ fn main() {
             };
             println!("Invalid value! Input a float number. Try again.");
         };
+    
         let superior_limit: f32 = loop {
             let superior_limit_message: String = format!("Input the superior limit of operation nº {}: ", limit_input);
             if let Ok(superior_limit_message_validity) = input(&superior_limit_message) {
@@ -40,13 +44,14 @@ fn main() {
             };
             println!("Invalid value! Input a float number. Try again.");
         };
+
         let mut limits_operation: Vec<f32> = Vec::new();
         limits_operation.push(inferior_limit);
         limits_operation.push(superior_limit);
         limits_operations.push(limits_operation);
-    }
     
+    }
 
-    println!("{:?}", operations);
+    integral::calculate(operations, limits_operations);
 
 }
