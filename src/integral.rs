0d7488f8a803017f06d2b2b8ec949d4a478a8f84
Marcos Felipe <mathcosfelipe@gmail.com>
2022-07-14 09:35:47 -0300

    Update integral.rs

--- src/integral.rs
+++ src/integral.rs
@@ -1,3 +1,19 @@
+use std::any::Any;
+
+pub trait Object {
+    fn type_name(&self) -> &str;
+    fn as_any(&self) -> &dyn Any;
+}
+
+impl Object for f32 {
+    fn type_name(&self) -> &str {"f32"}
+    fn as_any(&self) -> &dyn Any {self}
+}
+
+pub fn is_of_type<T: 'static>(x: &dyn Object) -> bool {
+    x.as_any().is::<T>()
+}
+
 fn integrate(operations: Vec<String>) -> Vec<String> {
 
     let mut operations_integrateds: Vec<String> = Vec::new();
@@ -14,8 +30,8 @@ fn integrate(operations: Vec<String>) -> Vec<String> {
                 operations_integrateds.push(("x^2/2").to_string());
             }
             
-            if operations[operation].contains("^") {
-                // let operation_splited
+            if is_of_type::<f32>(&operations[operation].parse::<f32>().unwrap()) {
+                operations_integrateds.push((format!("{}x", operations[operation])).to_string());
             }
 
         }
